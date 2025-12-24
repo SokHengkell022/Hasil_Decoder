07307b2ddc336c144f2ca040b013b9f595455cf9
By.Thonxyzz404 <thoniimodss@gmail.com>
2025-12-24 19:42:20 +0700

    Add files via upload

---
+++ Decrypt_nightmare_decript.py
@@ -0,0 +1,78 @@
+# Peta dekripsi, kebalikan dari peta enkripsi
+
+decryption_map = {
+
+    '﹅': 'A', '𐀳': 'B', '𓂀': 'C', '𐎋': 'D', '𐑀𐖬𐝒': 'E', '𐤟𐫦': 'F', '𐴵𐴵⅙': 'G', '𑊋𑊧𑊩': 'H',
+
+    '𝉅𝈣𝈣': 'I', '𒊹': 'J', '𒐇': 'K', '𒐹': 'L', '𒐳': 'M', '𒐑𒑅𒓄': 'N', '🀰': 'O', '🂠': 'P',
+
+    '𒐬𐂂': 'Q', 'ꤲ꥓': 'R', '꥟': 'S', '꧂꧁': 'T', '꧰ꧦꧦꧦ': 'U', 'ꥼ': 'V', '꫞': 'W', 'ꫛꫭ': 'X',
+
+    '': 'Y', 'J': 'Z',
+
+    '𝌈': '1', '𝌈𝌉': '2', '𝍖': '3', '𝌳': '4', '🨍🨙🨙': '5', '𒃯': '6', '𒅄': '7', '𒆔': '8',
+
+    '𒇺': '9', '𒈞': '0',
+
+    '': '+', '': '×', '': '÷', '': '=', '': '/', '': '_', '': '<', '': '>',
+
+    '': '[', '': ']', '': '!', '': '@', '': '#', '': '$', '': '%', '': '^',
+
+    '': '&', '': '*', 'i': '(', 'q': ')', 'bal': "'", '0': '"', '☆': ':',
+
+    '*': ';', '₩': '?', '﹆': ',', '𖹌': '.'
+
+}
+
+# Fungsi dekripsi
+
+def decrypt_text(text):
+
+    decrypted_text = ""
+
+    i = 0
+
+    max_len = max(len(k) for k in decryption_map.keys())  # Panjang simbol terenkripsi terpanjang
+
+    while i < len(text):
+
+        matched = False
+
+        # Coba cocokkan setiap simbol, mulai dari panjang terpanjang ke terpendek
+
+        for length in range(max_len, 0, -1):  # Mulai dari simbol terpanjang
+
+            if i + length <= len(text):
+
+                temp_char = text[i:i+length]  # Ambil bagian teks yang akan diuji
+
+                if temp_char in decryption_map:
+
+                    decrypted_text += decryption_map[temp_char]  # Cocokkan dan tambahkan ke hasil dekripsi
+
+                    i += length  # Lewati karakter yang sudah didekripsi
+
+                    matched = True
+
+                    break
+
+        if not matched:
+
+            decrypted_text += text[i]  # Jika tidak ditemukan, tambahkan karakter asli
+
+            i += 1
+
+    
+
+    return decrypted_text
+
+# Meminta input teks terenkripsi dari pengguna
+
+input_text = input("Enter the text you want to decrypt: ")
+
+# Menampilkan hasil dekripsi
+
+decrypted_output = decrypt_text(input_text)
+
+print("CONGRATULATIONS YOUR TEXT HAS BEEN DECRYPTED:", decrypted_output)
+__import__('sys').exit()
---
+++ Decrypt_nightmare_encrypt.py
@@ -0,0 +1,116 @@
+import os
+
+# Fungsi untuk membersihkan layar
+
+def clear_screen():
+
+    os.system('clear')
+
+# Fungsi untuk menambahkan warna RGB ke teks
+
+def color_text_rgb(r, g, b, text):
+
+    return f"\033[38;2;{r};{g};{b}m{text}\033[0m"
+
+# ANSI escape code for yellow
+
+YELLOW = "\033[93m"
+
+RESET = "\033[0m"
+
+# Banned text
+
+banned_text = """
+
+    _   ___       __    __                            ______                 __ 
+
+   / | / (_)___ _/ /_  / /_____ ___  ____ _________  / ____/______  ______  / /_
+
+  /  |/ / / __ `/ __ \/ __/ __ `__ \/ __ `/ ___/ _ \/ /   / ___/ / / / __ \/ __/
+
+ / /|  / / /_/ / / / / /_/ / / / / /_/ / /  /  __/ /___/ /  / /_/ / /_/ / /_  
+
+/_/ |_/_/\__, /_/ /_/\__/_/ /_/ /_/\__,_/_/   \___/\____/_/   \__, / .___/\__/  
+
+        /____/                                               /____/_/           
+
+"""
+
+# Peta enkripsi dengan simbol yang diperbaiki
+
+encryption_map = {
+
+    'A': '﹅', 'B': '𐀳', 'C': '𓂀', 'D': '𐎋', 'E': '𐑀𐖬𐝒', 'F': '𐤟𐫦', 'G': '𐴵𐴵⅙', 'H': '𑊋𑊧𑊩',
+
+    'I': '𝉅𝈣𝈣', 'J': '𒊹', 'K': '𒐇', 'L': '𒐹', 'M': '𒐳', 'N': '𒐑𒑅𒓄', 'O': '🀰', 'P': '🂠',
+
+    'Q': '𒐬𐂂', 'R': 'ꤲ꥓', 'S': '꥟', 'T': '꧂꧁', 'U': '꧰ꧦꧦꧦ', 'V': 'ꥼ', 'W': '꫞', 'X': 'ꫛꫭ',
+
+    'Y': '', 'Z': 'J',
+
+    '1': '𝌈', '2': '𝌈𝌉', '3': '𝍖', '4': '𝌳', '5': '🨍🨙🨙', '6': '𒃯', '7': '𒅄', '8': '𒆔',
+
+    '9': '𒇺', '0': '𒈞',
+
+    '+': '', '×': '', '÷': '', '=': '', '/': '', '_': '', '<': '', '>': '',
+
+    '[': '', ']': '', '!': '', '@': '', '#': '', '$': '', '%': '', '^': '',
+
+    '&': '', '*': '', '(': 'i', ')': 'q', "'": "bal", '"': '0', ':': '☆',
+
+    ';': '*', '?': '₩', ',': '﹆', '.': '𖹌'
+
+}
+
+# Fungsi enkripsi
+
+def encrypt_text(text):
+
+    encrypted_text = ""
+
+    for char in text.upper():
+
+        if char in encryption_map:
+
+            encrypted_text += encryption_map[char]
+
+        else:
+
+            encrypted_text += char
+
+    return encrypted_text
+
+# Bersihkan layar
+
+clear_screen()
+
+# Menampilkan teks banned dengan warna RGB merah
+
+colored_banned_text = color_text_rgb(255, 0, 0, banned_text)
+
+print(colored_banned_text)
+
+# Garis pemisah untuk memperindah tampilan
+
+print("=" * 60)
+
+# Sambutan
+
+print("𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐭𝐨 𝐭𝐡𝐞 𝐞𝐧𝐜𝐫𝐲𝐩𝐭𝐢𝐨𝐧 𝐭𝐨𝐨𝐥𝐬!")
+
+# Meminta input teks dari pengguna
+
+input_text = input("Enter Text To Encrypt: ")
+
+# Menampilkan hasil enkripsi dengan warna kuning
+
+encrypted_output = encrypt_text(input_text)
+
+print(f"{YELLOW}The Encrypted text: {encrypted_output}{RESET}")
+
+print("Free decryption tool please visit my youtube copyrect=@rendyx_solo-player")
+
+# Garis pemisah akhir
+
+print("=" * 60)
+__import__('sys').exit()
